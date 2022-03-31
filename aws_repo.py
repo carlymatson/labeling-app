db87e3757dc7b412ba4c168ce0bcd48d375195d7
Carly Matson <carlymatson21@gmail.com>
2022-03-31 16:07:58 -0400

    feat: Write credentials file.

--- aws_repo.py
+++ aws_repo.py
@@ -1,32 +1,73 @@
-from typing import Optional
-from typing import Dict
+import re
+from typing import Optional, Dict
 import json
+import toml
+import os
 
 import boto3
-
-session = boto3.Session(profile_name="daupler-annotation")
-lambda_client = session.client("lambda")
+import streamlit as st
 
 
-def invoke(lambda_func: str, params: Dict, invocation_type: str = "Event") -> Dict:
-    payload = json.dumps(params).encode("utf-8")
-    response = lambda_client.invoke(
-        FunctionName=lambda_func, InvocationType=invocation_type, Payload=payload
+def write_credentials_if_needed():
+    if "credentials_file_written" in st.session_state:
+        print("Credentials file has already been written.")
+        return
+    print("Writing credentials file...")
+    access_key_id = os.environ.get("AWS_ACCESS_KEY_ID", "")
+    secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    shared_creds_file = os.environ.get("AWS_SHARED_CREDENTIALS_FILE", "")
+    write_creds(access_key_id, secret_access_key, shared_creds_file)
+    st.session_state["credentials_file_written"] = True
+    print("File written.")
+
+
+def write_creds(access_key_id, secret_key, filename):
+    default = dict(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_key,
+        region="us-east-1",
     )
-    if invocation_type == "RequestResponse":
-        payload = response["Payload"].read().decode("utf-8")
-        return json.loads(payload)
-    return response
+    role = dict(
+        role_arn="arn:aws:iam::240316015535:role/developers",
+        source_profile="default",
+        region="us-east-1",
+    )
+    data = {"default": default, "daupler-production": role}
+    s = toml.dumps(data)
+    with open(filename, "w") as f:
+        stripped_of_quotes = re.sub('"', "", s)
+        f.write(stripped_of_quotes)
 
 
 class AWSRepository:
+    def __init__(self):
+        write_credentials_if_needed()
+        session = boto3.Session(profile_name="daupler-production")
+        lambda_client = session.client("lambda")
+        self.lambda_client = lambda_client
+
+    # FIXME Add retries on timeout
+    def invoke(
+        self, lambda_func: str, params: Dict, invocation_type: str = "Event"
+    ) -> Dict:
+        payload = json.dumps(params).encode("utf-8")
+        response = self.lambda_client.invoke(
+            FunctionName="daupler_annotation_" + lambda_func,
+            InvocationType=invocation_type,
+            Payload=payload,
+        )
+        if invocation_type == "RequestResponse":
+            payload = response["Payload"].read().decode("utf-8")
+            return json.loads(payload)
+        return response
+
     def get_batch(
         self,
         batch_size: int = 20,
         session_tag: Optional[str] = None,
         labeler_id: str = "",
     ):
-        batch = invoke(
+        batch = self.invoke(
             "PullBatchForLabeling",
             params=dict(
                 session_tag=session_tag, labeler_id=labeler_id, batch_size=batch_size
@@ -41,9 +82,9 @@ class AWSRepository:
             tag_string = " ".join(hashtags)
             if len(tag_string) > 0:
                 label["notes"] = label["notes"] + " " + tag_string
-        invoke(
+        self.invoke(
             "ProposeLabel",
             params=dict(
-                labeler_id=labeler_id, session_tag=session_tag, proposals=proposals
+                labeler_id=labeler_id, session_tag=session_tag, labeled_data=proposals
             ),
         )
